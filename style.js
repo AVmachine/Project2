86448c682637a6307a5884b55235b41089cfefae
avmachine <alex.av936@gmail.com>
2019-10-27 18:16:50 -0500

    Done!

--- style.js
+++ style.js
@@ -4,35 +4,41 @@ function changeBackground(newBackground){
         case 0:
             $("body").css("background-image","url('img/bg.jpg')");
             showMenu();
+            $("#bigText").html("<div id='bigText' class='bigTextClass'>Home<br>Find Peace<br>_</div>");
         break;
         case 1:
             $("body").css("background-image","url('img/bg1.jpg')");
             showMenu();
+            $("#bigText").html("<div id='bigText' class='bigTextClass'>Beach<br>Lift Yourself<br>_</div>");
         break;
         case 2:
             $("body").css("background-image","url('img/bg2.jpg')");
             showMenu();
+            $("#bigText").html("<div id='bigText' class='bigTextClass'>Ocean<br>Uncover Beauty<br>_</div>");
         break;
         case 3:
             $("body").css("background-image","url('img/bg3.jpg')");
             showMenu();
+            $("#bigText").html("<div id='bigText' class='bigTextClass'>Desert<br>Find Yourself<br>_</div>");
         break;
         case 4:
             $("body").css("background-image","url('img/bg4.jpg')");
             showMenu();
+            $("#bigText").html("<div id='bigText' class='bigTextClass'>Explore<br>Find Peace<br>_</div>");
         break;
         case 5:
             $("body").css("background-image","url('img/bg5.jpg')");
             showMenu();
+            $("#bigText").html("<div id='bigText' class='bigTextClass'>Mountains<br>Explore Limits<br>_</div>");
         break;
     }
 }
 
 
 
-// $(function(){
-// $("img").hide();
-// })
+$(function(){
+$("#wordsbg").hide();
+ })
 
 
 count = 0;
@@ -41,18 +47,27 @@ function showMenu()
     
     count += 1;
     if(count%2 == 1){
-        // $("img").fadeIn(3000);
         $("#imgbg").toggleClass("imgbgtrans");
         $("#img1").toggleClass("img1trans");
         $("#img2").toggleClass("img2trans");
         $("#img3").toggleClass("img3trans");
         $("#img4").toggleClass("img4trans");
         $("#img5").toggleClass("img5trans");
-
-
-
-        
-    }
+        $("#wordsbg").fadeIn(1500);
+        $("#words1").fadeIn(1500); 
+        $("#words2").fadeIn(1500); 
+        $("#words3").fadeIn(1500); 
+        $("#words4").fadeIn(1500); 
+        $("#words5").fadeIn(1500); 
+        $("#wordsbg").toggleClass("wordsBgTrans");
+        $("#words1").toggleClass("words1Trans");
+        $("#words2").toggleClass("words2Trans");
+        $("#words3").toggleClass("words3Trans");
+        $("#words4").toggleClass("words4Trans");
+        $("#words5").toggleClass("words5Trans");
+       
+     
+       }
     else{
         $("#imgbg").toggleClass("imgbgtrans");
         $("#img1").toggleClass("img1trans");
@@ -60,9 +75,20 @@ function showMenu()
         $("#img3").toggleClass("img3trans");
         $("#img4").toggleClass("img4trans");
         $("#img5").toggleClass("img5trans");
-
-
-
-        // $("img").fadeOut(3000);
+        $("#wordsbg").fadeOut();
+        $("#words1").fadeOut(); 
+        $("#words2").fadeOut(); 
+        $("#words3").fadeOut(); 
+        $("#words4").fadeOut(); 
+        $("#words5").fadeOut(); 
+        $("#wordsbg").toggleClass("wordsBgTrans");
+        $("#words1").toggleClass("words1Trans");
+        $("#words2").toggleClass("words2Trans");
+        $("#words3").toggleClass("words3Trans");
+        $("#words4").toggleClass("words4Trans");
+        $("#words5").toggleClass("words5Trans");
+        
     }
+
+
 }
